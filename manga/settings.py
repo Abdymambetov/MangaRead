e2ce8083e3f52296afb57a3821abd2f8707d5e26
Abdymambetov <meder0555@icloud.com>
2024-01-14 19:11:39 +0600

    parts correction

--- manga/settings.py
+++ manga/settings.py
@@ -22,12 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY = 'django-insecure-xsj5_gq_60c4n11uvt_#*23gqyxy2oz4_q!$f6kyd5(1okd+o7'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
-# DEBUG = True if os.environ.get('DEBUG') == 'on' else False
+DEBUG = True if os.environ.get('DEBUG') == 'on' else False
 
 ALLOWED_HOSTS = ['*']
 
